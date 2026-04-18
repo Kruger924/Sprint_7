29734aa12d206f162f075ec7a37699241b4d05e4
Kruger924 <kruger924@yandex.ru>
2026-04-18 22:29:07 +0300

    create test_orders_list

---
+++ tests/test_orders_list.py
@@ -0,0 +1,14 @@
+import allure
+import requests
+from data.endpoints import Endpoints
+from data.urls import Urls
+
+
+class TestOrdersList:
+
+    @allure.title('Проверка получения списка заказов')
+    @allure.description('Получение списка заказов и проверка ответа')
+    def test_list_orders_success(self):
+        response = requests.get(f'{Urls.QA_SCOOTER_URL}{Endpoints.get_orders_list}')
+        assert response.status_code == 200
+        assert "track" in response.text
