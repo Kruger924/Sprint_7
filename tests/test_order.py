6a6b74f25bc8aa7f882f0f289abf61367ddccda3
Kruger924 <kruger924@yandex.ru>
2026-04-20 21:08:13 +0300

    edit color in test_order

--- tests/test_order.py
+++ tests/test_order.py
@@ -11,7 +11,7 @@ class TestOrderCreate:
 
     @allure.title('Проверка оформления заказа с различными цветами самоката')
     @allure.description('Отправка запроса на создание заказа с поочередным добавлением различных цветов и проверка ответа')
-    @pytest.mark.parametrize('color', [{"color": ["BLACK"]}, {"color": ["GREY"]}, {"color": ["BLACK", "GRAY"]}, {"color": [""]}])
+    @pytest.mark.parametrize('color', [{"color": ["BLACK"]}, {"color": ["GREY"]}, {"color": ["BLACK", "GREY"]}, {"color": [""]}])
     def test_create_order_success(self, color):
         headers = {"Content-type": "application/json"}
         data = DataOrder.data
